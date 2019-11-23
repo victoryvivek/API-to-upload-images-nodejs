fb15f914702a82a27ce65eb128cd2dc68cb8363e
victoryvivek <vivekaggarwalthegreat1998@gmail.com>
2019-11-23 21:42:21 +0530

    Finally image uploaded

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const CLOUDINARY_PRESET = "ub8x4sqg";
 cloudinary.config({
   cloud_name: "dxjw59s8q",
   api_key: "368959491475955",
-  api_secret: "mSsNmFWiWXb2-0hmq6PtgFoE4xM,"
+  api_secret: "mSsNmFWiWXb2-0hmq6PtgFoE4xM"
 });
 
 const app = express();
@@ -43,53 +43,50 @@ app.get("/home", (req, res, next) => {
   const url = req.query.val;
   console.log("url : " + url);
 
-  var name= new Date().toISOString()+'.jpg';
-  let storage=path.resolve(__dirname,'files','images.jpg');
-    console.log(storage);
-    let x = storage.toString();
-    console.log(x);
+  var name = new Date().toISOString() + ".jpg";
+  let storage = path.resolve(__dirname, "files", "images.jpg");
+  console.log(storage);
+  let x = storage.toString();
+  console.log(x);
   axios({
     url: url,
-    method: 'GET',
-    responseType:'stream'
-  }).then(resp=>{
-      resp.data.pipe(fs.createWriteStream(storage));
+    method: "GET",
+    responseType: "stream"
+  }).then(resp => {
+    resp.data.pipe(fs.createWriteStream(storage));
       cloudinary.uploader.upload(x,function(err,result){
           console.log(err);
-        
-        
+
       });
-  }).catch(err=>{
-      console.log(err);
   });
 
-//   https
-//     .get(url, resp => {
-//       let data = "";
+  //   https
+  //     .get(url, resp => {
+  //       let data = "";
 
-//       resp.on("data", d => {
-//         data += d;
-//       });
+  //       resp.on("data", d => {
+  //         data += d;
+  //       });
 
-//     //   var parseFile = new Parse.File('landscape');
-//     //   parseFile.save().then(()=>{
-//     //       console.log('URL : '+parseFile.url());
-//     //   }).catch(err=>{
-//     //       console.log(err);
-//     //   });
+  //     //   var parseFile = new Parse.File('landscape');
+  //     //   parseFile.save().then(()=>{
+  //     //       console.log('URL : '+parseFile.url());
+  //     //   }).catch(err=>{
+  //     //       console.log(err);
+  //     //   });
 
-//       var formData = new FormData();
-//       formData.append("file", data);
-//       formData.append("upload_preset", CLOUDINARY_PRESET);
+  //       var formData = new FormData();
+  //       formData.append("file", data);
+  //       formData.append("upload_preset", CLOUDINARY_PRESET);
 
-//     //   cloudinary.uploader.upload('',function(err,result){
+  //     //   cloudinary.uploader.upload('',function(err,result){
 
-//     //   });
+  //     //   });
 
   return res.json({
     message: "Request Procesing"
   });
-
+});
 
 // app.get('/home',(req,res,next)=>{
 //     const url = req.query.val;
@@ -131,4 +128,3 @@ app.get("/home", (req, res, next) => {
 //     });
 // });
 
-});
